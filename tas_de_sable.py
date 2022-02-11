a8ffde6cddd113b35a66a31e87ab392207c95fde
Elta305 <71965677+Elta305@users.noreply.github.com>
2022-02-11 15:32:05 +0100

    Répartition du code
    Séparation des différentes parties du code.

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -6,3 +6,28 @@
 # Manel Mokrab
 # https://github.com/uvsq-info/l1-python
 ########################
+
+# import des librairies
+
+import tkinter as tk
+import random as rd
+
+
+########################
+# constantes
+
+
+####################
+# fonctions
+
+
+# Presets
+
+
+#########################
+# partie principale
+
+# création des widgets
+
+
+# Presets
