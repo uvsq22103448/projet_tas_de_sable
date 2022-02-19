003188c1aee7fa91f959bf6424977da610ac11b9
Lisa VAUVERT <lisa.vauvert@ens.uvsq.fr>
2022-02-19 19:24:09 +0100

    update fonctions avec liste

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -20,6 +20,41 @@ import random as rd
 ####################
 # fonctions
 
+#Faire des listes de lite 
+
+l = [[4, 5, 2], [4, 3, 0], [1, 5, 4]]
+
+N = 5 
+
+def initialisation():
+    grid = []
+    
+    for i in range(N-2):
+        grid.append(["#"])
+        for j in range(N-2):
+            grid[i].append(0)
+        grid[i].append("#")
+    grid.insert(0, ["#"]*N)
+    grid.append(["#"]*N)
+
+    return affichage(grid)
+
+def affichage(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            canvas.create_text(50, 50, text = str(grid[i][j]))
+     
+
+root = tk.Tk()
+
+canvas = tk.Canvas(root, height=500, width = 500)
+canvas.grid(column = 0, row = 0)
+
+
+
+
+initialisation()
+root.mainloop()
 
 # Presets
 
